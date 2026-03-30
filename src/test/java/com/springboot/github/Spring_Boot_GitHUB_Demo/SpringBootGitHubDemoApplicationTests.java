213582c108481f2e0ef5676f28a09b2ef5b18701
Nitin <nitinkumarmahli3@gmail.com>
2026-03-30 09:55:29 +0530

    file changed

---
+++ src/test/java/com/springboot/github/Spring_Boot_GitHUB_Demo/SpringBootGitHubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.github.Spring_Boot_GitHUB_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitHubDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
